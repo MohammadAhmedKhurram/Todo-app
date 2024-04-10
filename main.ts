5da32799726beb96d678f6d77923a36bbb7ac177
MohammadAhmedKhurram <162457856+MohammadAhmedKhurram@users.noreply.github.com>
2024-04-10 14:29:12 +0500

    Todo app

---
+++ main.ts
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let todos: string[] = [];
+
+console.log(chalk.bold.red.bgYellow('Welcome To The Only Choice Todo App'));
+
+let condition = true;
+while (condition) {
+    let todoQuestion = await inquirer.prompt([
+        {
+            name: "firstQuestion",
+            type: "input",
+            message: chalk.yellow("What would you like to add in your todos?")
+        },
+        {
+            name: "secondQuestion",
+            type: "confirm",
+            message: chalk.blue("Would you like to add more in your todos?"),
+            default: true
+        }
+    ]);
+
+    todos.push(todoQuestion.firstQuestion);
+    console.log(chalk.green("Updated todos:"));
+    todos.forEach(todo => console.log(chalk.cyan(todo))); // Print each todo item in cyan color
+    condition = todoQuestion.secondQuestion;
+}
